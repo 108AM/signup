102406a5e5e165c765b7db8fc59c71153e6111a8
Asmee Mishra <asmeemishra@asmees-air.home>
2024-10-02 16:10:26 +0100

    checkpoint

--- connection.php
+++ connection.php
@@ -6,22 +6,17 @@
 </head>
 <body>
     <form action="process-signup.php" method="post"> 
-        <div>
-            <label for= "name">Name</label>
-            <input type="text" id="name" name="name">
-        </div>
-        <div>
-            <label for="email">Email</label> 
-            <input type="email" id="email" name="email">
-        </div>
-        <div>
-            <label for="password">Password</label>
-            <input type="password" id="password" name="password">
-        </div>
-        <div>
-            <label for="password confirmation">Repeat password</label> 
-            <input type="password" id="password confirmation" name="password confirmation">
-        </div>
+        <label for= "name">Name</label>
+        <input type="text" id="name" name="name">
+    
+        <label for="email">Email</label> 
+        <input type="email" id="email" name="email">
+    
+        <label for="password">Password</label>
+        <input type="password" id="password" name="password">
+    
+        <label for="password confirmation">Repeat password</label> 
+        <input type="password" id="password confirmation" name="password confirmation">
     </form>
     <button>Submit</button>
 </body>
--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
-    <link rel="stylesheet" sheet="style.css">
+    <link rel="stylesheet" href="style.css">
     <title>Hello, world!</title>
   </head>
   <body>
@@ -25,10 +25,12 @@
                 <input type="email" id="email" name="email"><br><br>
             </div>
             <div>
+                <i class="fa-solid fa-lock"></i>
                 <label for="password">Password</label>
                 <input type="password" id="password" name="password"><br><br>
             </div>
             <div>
+                <i class="fa-solid fa-lock"></i>
                 <label for="password confirmation">Repeat password</label> 
                 <input type="password" id="password confirmation" name="password confirmation"><br><br>
             </div>
